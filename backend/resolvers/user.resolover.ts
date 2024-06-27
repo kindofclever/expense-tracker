18ae9d8d2944d73d8e86fc890748b19f78519afa
Sandra <sandra.gufler@gmx.ch>
2024-06-27 18:00:37 +0200

    feat: add user query

--- backend/resolvers/user.resolover.ts
+++ backend/resolvers/user.resolover.ts
@@ -1,10 +1,15 @@
 import { users } from "../dummyData/data.ts"
+import { IResolvers } from "@graphql-tools/utils"
 
-const userResolver = {
+const userResolver: IResolvers = {
   Query: {
     users: () => {
       return users
+    },
+    user: (_, args: { userId: string }) => {
+      return users.find(user => user._id === args.userId);
     }
+
   },
 
   Mutation: {}
